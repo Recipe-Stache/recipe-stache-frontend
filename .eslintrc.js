0146cffa08b15a337bf64bd5cedd47a7eaf4b7ad
Derek <dhass421@gmail.com>
2021-09-03 21:56:56 -0400

    Dockerized frontend and fixed eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'no-tabs': ['error', { allowIndentationTabs: true }],
-		indent: [2, 'tab'],
-		semi: 'always'
-  }
-}
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'standard'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: [
+		'react'
+	],
+	ignorePatterns: ['App.js', 'reportWebVitals.js', 'index.js'],
+	rules: {
+		'no-tabs': ['warn', { allowIndentationTabs: true }],
+		indent: [1, 'tab'],
+		semi: [1, 'always'],
+		'space-before-function-paren': ['warn', 'never']
+	}
+};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-ui/core/styles'
-import { theme } from './Util/theme'
+import theme from './Util/theme'
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />;
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
 
