5250516c1bf1cdd145dac40c6f79297e4a194a98
joshua-montanari <joshua.montanari@gmail.com>
2022-01-19 23:13:20 -0500

    [RS-2] Added TS and Chakra

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './Components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header title={"Josh and derek try TS"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Heading } from '@chakra-ui/react'
+
+type props = {
+  title: string
+}
+
+const Header:FC<props> = ({ title }) => {
+  return <Heading>{title}</Heading>
+}
+
+export default Header
