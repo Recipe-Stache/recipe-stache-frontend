209cbcd1a203f774804ede65903f82a2244460ab
jmmontanari <joshua.montanari@gmail.com>
2022-01-22 22:56:51 -0500

    [RS-Router] Added react router and starter pages for routes

---
+++ src/Pages/Create.tsx
@@ -0,0 +1,11 @@
+import Header from '../Components/Header';
+
+function Create() {
+  return (
+    <div>
+      <Header title={"Create Recipes Here!!"}/>
+    </div>
+  );
+}
+
+export default Create;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from '../Components/Header';
+
+function Home() {
+  return (
+    <div>
+      <Header title={"Check Out These Recipes!"}/>
+    </div>
+  );
+}
+
+export default Home;
